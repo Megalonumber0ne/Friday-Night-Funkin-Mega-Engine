86d4902dd812dcf918146fc0bacec6c4c2135c27
Megalo <87905744+Megalonumber0ne@users.noreply.github.com>
2023-05-01 20:19:27 +0100

    Add files via upload

--- source/handlers/ClientPrefs.hx
+++ source/handlers/ClientPrefs.hx
@@ -10,12 +10,13 @@ import Controls;
 class ClientPrefs {
 	// For load and save.
 	static var settingNames:Array<String> = [
-		"OldGraphics", "GhostTapping"
+		"OldGraphics", "ghostTapping"
 	];
 	// Options
-	public static var ghostTapping:Bool = false;
+	public static var ghostTapping:Bool = true;
 	public static var oldGraphics:Bool = false;
 	public static var comboSplash:Bool = true;
+	public static var showComboSprite:Bool = true;
 
 	public static var leftKeybinds:Array<FlxKey> = [FlxKey.A, FlxKey.LEFT];
 	public static var downKeybinds:Array<FlxKey> = [FlxKey.S, FlxKey.DOWN];
@@ -24,8 +25,6 @@ class ClientPrefs {
 	public static var resetKeybind:FlxKey = FlxKey.R;
 	public static final controls:Controls = new Controls("player0", Solo);
 
-	public static var OldGraphics:Bool = false; 
-	public static var showComboSprite:Bool = true;
 	public static var antialiasing:Bool = true;
 	public static var framerate:Int = 60;
 
@@ -48,19 +47,22 @@ class ClientPrefs {
 	public static var defaultFont:String = 'vcr';
 
 	public static function saveSettings() {
-		for (setting in settingNames) {
-			Reflect.setField(FlxG.save.data, setting, Reflect.getProperty(ClientPrefs, setting));
-		}
 		FlxG.save.data.framerate = framerate;
 		#if (flixel > "5.0.0")
 		FlxSprite.defaultAntialiasing = antialiasing;
 		#end
-
+		
 		FlxG.save.flush();
 
+		var settingsSave:FlxSave = new FlxSave();
+		settingsSave.bind('settings', 'Funkin-0.2.2-Engine-Save');
+		settingsSave.data.ghostTapping = ghostTapping;
+		settingsSave.data.comboSplash = comboSplash;
+		settingsSave.data.oldGraphics = oldGraphics;
+		settingsSave.flush();
+
 		var controlSave:FlxSave = new FlxSave();
-		controlSave.bind('controls',
-			'Funkin-0.2.2-Engine-Save'); // Placing this in a separate save so that it can be manually deleted without removing your Score and stuff
+		controlSave.bind('controls', 'Funkin-0.2.2-Engine-Save'); // Placing this in a separate save so that it can be manually deleted without removing your Score and stuff
 		controlSave.data.leftBinds = leftKeybinds;
 		controlSave.data.downBinds = downKeybinds;
 		controlSave.data.upBinds = upKeybinds;
@@ -71,12 +73,16 @@ class ClientPrefs {
 	}
 
 	public static function loadPrefs() {
-		FlxG.save.bind('funkin', 'Funkin-0.2.2-Engine-Save');
-		for (setting in settingNames) {
-			var savedData = Reflect.field(FlxG.save.data, setting);
-			if (savedData != null)
-				Reflect.setProperty(ClientPrefs, setting, savedData);
-		}
+		var settingsSave:FlxSave = new FlxSave();
+		settingsSave.bind('settings', 'Funkin-0.2.2-Engine-Save');
+        if(settingsSave.data.comboSplash != null)
+			comboSplash = settingsSave.data.comboSplash;
+        if(settingsSave.data.oldGraphics != null)
+			oldGraphics = settingsSave.data.oldGraphics;
+        if(settingsSave.data.ghostTapping != null)
+			ghostTapping = settingsSave.data.ghostTapping;
+		if(settingsSave.data.ghostTapping == null)
+			trace('THIS IS NULL');
 
 		var controlSave:FlxSave = new FlxSave();
 		controlSave.bind('controls', 'Funkin-0.2.2-Engine-Save');
